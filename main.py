22170b24457623bb0aa1285b59466bd0d370c8d8
daniel <danielfettsack>
2024-11-01 22:57:32 +0100

    ability to scan after number of listens to a certain artist

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 from tkinter import *
 from tkinter import filedialog as fd
 from tkinter import ttk
+import json
+import os
+from sqlite3 import *
 
 root = Tk()
 root.title("SpotifyListener")
 label = Label(root, text="Select the data Folder")
-entry = Entry(root, fg="green", bg="blue")
+entry = Entry(root)
 
-def select_file():
-    filename = fd.askopenfilename()
+def load_file():
+    counter = 0
+    directory_name = fd.askdirectory()
+    for filename  in os.listdir(directory_name):
+        if filename.endswith(".json") and filename.startswith("Streaming_History_Audio"):
+            with open (directory_name + "/" + filename, 'r', encoding="utf8") as file:
+                song_directory_list = json.load(file)
+                for song_instance in song_directory_list:
+                    if song_instance["master_metadata_album_artist_name"] == "Travis Scott":
+                        counter = counter + 1
+                        print(counter)
+                        print(song_instance["master_metadata_album_artist_name"])
+
 open_button = ttk.Button(
     root,
     text="File",
-    command= select_file
+    command= load_file
 )
 
 label.pack()
