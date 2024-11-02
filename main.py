073f4b325b1fb631aa49e768eaaec7f7b672e0bd
daniel <danielfettsack>
2024-11-02 20:38:56 +0100

    Added ability to convert JSON to db file

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import tkinter as tk
 from enum import Enum
 import json
 import os
-from sqlite3 import *
+import sqlite3
 
 root = Tk()
 root.title("SpotifyListener")
@@ -47,8 +47,66 @@ def load_file():
                         counter = counter + 1
     print(counter)
 
+def get_folder_path():
+   print(fd.askdirectory())
+   folder_path = fd.askdirectory()
 
 
+def load_files_into_db():
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS spotifyData (
+            artist_name TEXT NOT NULL,
+            track_name TEXT NOT NULL,
+            album_name TEXT NOT NULL,
+            ms_listened INTEGER NOT NULL
+        )
+    ''')
+    conn.commit()
+
+    directory_name = fd.askdirectory()
+    song_count = 0
+
+    for filename in os.listdir(directory_name):
+        if filename.endswith(".json") and filename.startswith("Streaming_History_Audio"):
+            file_path = os.path.join(directory_name, filename)
+            with open(file_path, 'r', encoding="utf8") as file:
+                song_directory_list = json.load(file)
+
+                for song_instance in song_directory_list:
+                    if all(key in song_instance and song_instance[key]
+                           for key in [
+                               "master_metadata_track_name",
+                               "master_metadata_album_artist_name",
+                               "master_metadata_album_album_name",
+                               "ms_played"]):
+
+                        cursor.execute('''
+                            INSERT INTO spotifyData (artist_name, track_name, album_name, ms_listened)
+                            VALUES (:artist_name, :track_name, :album_name, :ms_listened)
+                        ''', {
+                            'artist_name': song_instance["master_metadata_album_artist_name"],
+                            'track_name': song_instance["master_metadata_track_name"],
+                            'album_name': song_instance["master_metadata_album_album_name"],
+                            'ms_listened': song_instance["ms_played"]
+                        })
+                        song_count += 1
+                    else:
+                        print(f"Ein Eintrag in {filename} wurde übersprungen, da er unvollständig oder leer ist.")
+
+    conn.commit()
+    conn.close()
+
+    print(f"{song_count} Songs erfolgreich in die Datenbank eingefügt.")
+
+
+transfer_to_db_button = ttk.Button(
+    root,
+    text="transfer to db",
+    command=load_files_into_db
+)
 
 open_button = ttk.Button(
     root,
@@ -56,6 +114,7 @@ open_button = ttk.Button(
     command=load_file
 )
 
+transfer_to_db_button.pack()
 open_button.pack()
 name = entry.get()
 root.mainloop()
